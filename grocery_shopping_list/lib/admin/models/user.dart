e032f72dd9ea5a3d0d3a3467c26f2bfbc336b231
Elizabet Yonas <elizabethyonas700@gmail.com>
2024-04-20 10:20:28 +0300

    admin page

---
+++ grocery_shopping_list/lib/admin/models/user.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class User{
+  final String username;
+  final String location;
+  final List items;
+
+  User(this.username, this.location, this.items);
+}
